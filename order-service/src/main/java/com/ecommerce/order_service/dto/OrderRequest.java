2f1b8ef0ef161be900c53fbcfe40df820a84b8e3
theHARI962n <rajul1874@gmail.com>
2026-06-14 23:30:25 +0530

    chenged the order dto design

--- order-service/src/main/java/com/ecommerce/order_service/dto/OrderRequest.java
+++ order-service/src/main/java/com/ecommerce/order_service/dto/OrderRequest.java
@@ -1,24 +1,47 @@
 package com.ecommerce.order_service.dto;
 
-import java.util.UUID;
+
+import java.util.List;
+
 
 public class OrderRequest {
-    private UUID productId;
-    private Integer quantity;
 
-    public UUID getProductId() {
-        return productId;
+
+    private List<OrderItemRequest> items;
+
+
+    public List<OrderItemRequest> getItems() {
+        return items;
     }
 
-    public void setProductId(UUID productId) {
-        this.productId = productId;
-    }
 
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
+    public void setItems(List<OrderItemRequest> items) {
+        this.items = items;
     }
 }
+
+
+//package com.ecommerce.order_service.dto;
+//
+//import java.util.UUID;
+//
+//public class OrderRequest {
+//    private UUID productId;
+//    private Integer quantity;
+//
+//    public UUID getProductId() {
+//        return productId;
+//    }
+//
+//    public void setProductId(UUID productId) {
+//        this.productId = productId;
+//    }
+//
+//    public Integer getQuantity() {
+//        return quantity;
+//    }
+//
+//    public void setQuantity(Integer quantity) {
+//        this.quantity = quantity;
+//    }
+//}
