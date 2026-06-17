8fed9412c04bc7717b449fb2bc5d25cde2d95c3e
theHARI962n <rajul1874@gmail.com>
2026-06-17 20:36:43 +0530

    added dto orderitemrequest

---
+++ cart_service/src/main/java/com/ecommerce/cart_service/dto/OrderItemRequest.java
@@ -0,0 +1,34 @@
+package com.ecommerce.cart_service.dto;
+
+
+import java.util.UUID;
+
+
+public class OrderItemRequest {
+
+
+    private UUID productId;
+
+
+    private Integer quantity;
+
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+
+    public void setProductId(UUID productId) {
+        this.productId = productId;
+    }
+
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
