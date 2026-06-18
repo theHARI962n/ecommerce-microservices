c9d81e7a2eb908fba44020408bed7a426fd69736
theHARI962n <rajul1874@gmail.com>
2026-06-19 00:25:19 +0530

    added dto for update quantity

---
+++ cart_service/src/main/java/com/ecommerce/cart_service/dto/UpdateQuantityRequest.java
@@ -0,0 +1,14 @@
+package com.ecommerce.cart_service.dto;
+
+public class UpdateQuantityRequest {
+
+    private Integer quantity;
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
